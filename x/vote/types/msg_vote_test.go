de5463c9fb8dda49a3d74b2d7a2a3d5bfbd59bdf
David Tran <tranhuyducseven@gmail.com>
2025-04-01 14:14:49 +0700

    feat: write test for vote request

--- x/vote/types/msg_vote_test.go
+++ x/vote/types/msg_vote_test.go
@@ -3,41 +3,38 @@ package types
 import (
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/scalarorg/scalar-core/testutils/rand"
+	chains "github.com/scalarorg/scalar-core/x/chains/exported"
+	chainsTypes "github.com/scalarorg/scalar-core/x/chains/types"
+	covTypes "github.com/scalarorg/scalar-core/x/covenant/types"
 	nexus "github.com/scalarorg/scalar-core/x/nexus/exported"
 	"github.com/scalarorg/scalar-core/x/vote/exported"
-	chainsTypes "github.com/scalarorg/scalar-core/x/chains/types"
 )
 
+
+// CGO_LDFLAGS="-L./lib -lbitcoin_vault_ffi" CGO_CFLAGS="-I./lib" go test -timeout 10m -run ^TestVoteRequest_ValidateBasic$ github.com/scalarorg/scalar-core/x/vote/types -v -count=1
 func TestVoteRequest_ValidateBasic(t *testing.T) {
-	t.Run("no sender", func(t *testing.T) {
-		vote := NewVoteRequest(nil, exported.PollID(rand.PosI64()), chainsTypes.NewVoteEvents(nexus.ChainName(rand.NormalizedStr(3))))
-		assert.Error(t, vote.ValidateBasic())
-	})
+	t.Run("correct vote events", func(t *testing.T) {
+		chain := nexus.ChainName("bitcoin|4")
+		voteEvent := covTypes.NewVoteEvents(chain, covTypes.Event{
+			Chain: chain,
+			Hash:  nil,
+			Event: &covTypes.Event_RedeemTxsConfirmed{
+				RedeemTxsConfirmed: &covTypes.RedeemTxsConfirmed{
+					EventIDs: []chainsTypes.EventID{},
+					UtxoSnapshot: &covTypes.UTXOSnapshot{
+						CustodianGroupUID: chains.Hash(rand.Bytes(32)),
+						BlockHeight:       1, // TODO: fill me
+						Utxos:             []*covTypes.UTXO{},
+					},
+				},
+			},
+			Index: 0,
+		})
 
-	t.Run("no vote", func(t *testing.T) {
-		vote := &VoteRequest{
-			Sender: rand.AccAddr(),
-			PollID: exported.PollID(rand.PosI64()),
-			Vote:   nil,
-		}
-		assert.Error(t, vote.ValidateBasic())
-	})
-
-	t.Run("faulty vote type", func(t *testing.T) {
-		vote := &VoteRequest{
-			Sender: rand.AccAddr(),
-			PollID: exported.PollID(rand.PosI64()),
-			Vote:   types.UnsafePackAny("result"),
-		}
-		assert.Error(t, vote.ValidateBasic())
-	})
-
-	t.Run("correct vote", func(t *testing.T) {
-		vote := NewVoteRequest(rand.AccAddr(), exported.PollID(rand.PosI64()), chainsTypes.NewVoteEvents(nexus.ChainName(rand.NormalizedStr(3))))
+		vote := NewVoteRequest(rand.AccAddr(), exported.PollID(rand.PosI64()), voteEvent)
 		assert.NoError(t, vote.ValidateBasic())
 	})
 }
